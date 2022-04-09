3e92018c13fa191738c9f593868f98c0fc0af08c
Thomas <thomas.alexaline@gmail.com>
2022-04-09 12:53:31 +0200

    added script to manage card, working on memory maanger

---
+++ ReimynsMemories/Assets/Script/Phase2/Card/Card.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Card : MonoBehaviour
+{
+    [SerializeField] private string cardName;
+    [SerializeField] private string effectText;
+
+    [SerializeField] private Sprite cardFrontSprite;
+
+    public abstract void DoEffect(); //take hostile behaviour + player as parameter
+
+    public string GetCardName()
+    {
+        return cardName;
+    }
+
+    public string GetEffectText()
+    {
+        return effectText;
+    }
+
+    public Sprite GetCardFrontSprite()
+    {
+        return cardFrontSprite;
+    }
+}
