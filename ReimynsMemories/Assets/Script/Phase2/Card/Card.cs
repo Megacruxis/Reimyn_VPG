eb2c4012234d3a5036d193966cdef25ff440e721
Thomas <thomas.alexaline@gmail.com>
2022-04-09 14:49:28 +0200

    added code to shuffle deck and updated gameManager

--- ReimynsMemories/Assets/Script/Phase2/Card/Card.cs
+++ ReimynsMemories/Assets/Script/Phase2/Card/Card.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Card : MonoBehaviour
+public abstract class Card
 {
     [SerializeField] private string cardName;
     [SerializeField] private string effectText;
--- ReimynsMemories/Assets/Script/Phase2/MemoryCombatManager.cs
+++ ReimynsMemories/Assets/Script/Phase2/MemoryCombatManager.cs
@@ -13,9 +13,10 @@ public class MemoryCombatManager : MonoBehaviour
     [SerializeField] private int numberOfColumn;
 
     [Header("Emplacement where card can be positioned")]
-    [SerializeField] private Transform[] cardSlots;
+    [SerializeField] private CardDisplayManager[] cardSlots;
 
-    private bool[] emptycardSlots;
+    private List<int> emptycardSlots;
+    private int gridNumberOfSlots;
 
     private void Awake()
     {
@@ -29,26 +30,68 @@ public class MemoryCombatManager : MonoBehaviour
         }
         if(numberOfColumn <= 0)
         {
-
+            Debug.LogError("Error in script MemoryCombatManager, numberOfColumn = " + numberOfColumn 
+                + " but should be greater than 0");
         }
         if(numberOfColumn <= 0)
         {
-
+            Debug.LogError("Error in script MemoryCombatManager, numberOfLine = " + numberOfLine
+                + " but should be greater than 0");
         }
         if(cardSlots.Length < numberOfLine * numberOfColumn)
         {
-
+            Debug.LogError("Error in script MemoryCombatManager, not enought card slot for the given number of line and column");
         }
+
+        gridNumberOfSlots = numberOfLine * numberOfColumn;
+        if(gridNumberOfSlots % 2 == 1)
+        {
+            Debug.LogError("Error in script MemoryCombatManager, number of card slots should be an even number");
+        }
+
+        emptycardSlots = new List<int>();
     }
 
     private void Start()
     {
-        emptycardSlots = new bool[numberOfLine * numberOfColumn];
+        SetEmptyCardSlot();
+        FillGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetEmptyCardSlot()
     {
-        
+        emptycardSlots.Clear();
+        for (int i = 0; i < gridNumberOfSlots; i++)
+        {
+            emptycardSlots.Add(i);
+        }
+    }
+
+    /*
+     * Fill the grids with card from the player deck
+     */
+    private void FillGrid() 
+    {
+        for (int i = 0; i < gridNumberOfSlots/2; i++)
+        {
+            Card selectedCard = playerDeckSO.DrawNextCard();
+            if(selectedCard == null)
+            {
+                //maybe shuffle ?
+                Debug.LogError("Error in script MemoryCombatManager, could not fill the grid, deck is empty");
+                return;
+            }
+            for(int r = 0; r < 2; r++)
+            {
+                if(emptycardSlots.Count == 0)
+                {
+                    Debug.LogError("Error in script MemoryCombatManager, no empty card slot left");
+                    return;
+                }
+                int selectedSlot = emptycardSlots[Random.Range(0, emptycardSlots.Count)];
+                emptycardSlots.Remove(selectedSlot);
+                cardSlots[selectedSlot].SetCardInfo(selectedCard);
+            }
+        }
     }
 }
